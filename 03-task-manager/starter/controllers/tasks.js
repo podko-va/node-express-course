59eefc86cfe72e5d29f49e45e19cee5fe1db58f8
Kate <mykateWgmail.com>
2024-07-23 10:59:56 +0300

    Input Validation

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -4,8 +4,13 @@ const getAllTasks = (req,res) =>{
     res.send('all items on the screen');
 }
 const createTask = async (req,res) =>{
-    const task = await Task.create(req.body)
-    res.status(201).json({task});
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({task});
+        
+    } catch (error) {
+        res.status(500).json({msg:error}) 
+    }
 }
 const updateTask = (req,res) =>{
     res.send('task updated');
