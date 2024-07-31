73f53e3f87ccb4b439eb4f9ddb480efca9662031
Kate <mykateWgmail.com>
2024-07-31 11:17:55 +0300

    added sort,limit,page,skip

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,19 +3,21 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
     //throw new Error('testing async errors')
-    const search = 'be'
+    const search = 'e'
     
     const products = await Product.find({
         name: { $regex: search, $options: 'i'},
-    }).sort('name -price')
+    })
+    .sort('name -price')
+    .select('name')
+    .limit(6)
+    .skip(1);
     console.log({ $regex: search, $options: 'i'});
     res.status(200).json({products, nbHits: products.length})
 }
 
-const getAllProduct = async(req,res) => {
-    const products = await Product.find(req.query);
-    
-    const {featured, company, name} = req.query
+const getAllProduct = async(req,res) => {    
+    const {featured, company, name, sort, fields} = req.query
     const queryObject = {}
     if (featured){
         queryObject.featured = featured === 'true' ? true : false
@@ -27,6 +29,29 @@ const getAllProduct = async(req,res) => {
         queryObject.name = { $regex: name, $options: 'i'}
     }
     console.log(queryObject);
+
+    let result = Product.find(queryObject)
+    //sort
+    if (sort){
+        console.log(sort)
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList) 
+    }
+    else {
+        result = result.sort('createAt')
+    }
+    //field
+    if (fields){
+        console.log(fields)
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList) 
+    }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page-1)*limit;
+
+    result = result.skip(skip).limit(limit);
+    const products = await result;
     res.status(200).json({products, nbHits: products.length})
 }
 
